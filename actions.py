9430ae8e93936f3beb5dda0783aec87ccad87591
devwabi <mohamed.abdouwabi@gmail.com>
2020-06-03 15:53:02 +0000

    Dispatcher 2nd form

--- actions.py
+++ actions.py
@@ -43,8 +43,8 @@ class ActionYesNo(Action):
                     
                 ]
             }
-            dispatcher.utter_custom_json(message)
-
+           # dispatcher.utter_custom_json(message)
+    	    dispatcher.utter_message(json_message=message)
         else:
             dispatcher.utter_message(template="utter_greet")
         return []
